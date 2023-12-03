a73376630307665804cefeaa49824e7039ca94d9
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-12-02 22:06:39 -0600

    The power of Gary and mini Project part 3

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,32 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+  });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+  
+    if (!promptEvent) {
+      return;
+    }
+  
+    promptEvent.prompt();
+  
+    // TODO: Notice line 7 above.  window.deferredPrompt should now be set to null.
+  
+    // TODO: Notice line 8 above. We want the same line here, but the hidden value should be set to true.
+    // Insert line here
+
+    window.deferredPrompt = null;
+  
+    butInstall.classList.toggle('hidden', true);
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+  });
